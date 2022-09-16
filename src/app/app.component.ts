489428e1e52d8cb396e2846624da60b87dbb5538
weisheng-alcodes <neohws.alcodes@gmail.com>
2022-09-17 02:19:57 +0800

    Remove PrimeNG, added angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
 import { Platform } from '@angular/cdk/platform';
 import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
-import { filter, map } from 'rxjs';
-import { PrimeNGConfig } from 'primeng/api';
+import { filter, map, Observable, ReplaySubject } from 'rxjs';
+import { DataSource } from '@angular/cdk/collections';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 11, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 12, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 13, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 14, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 15, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 16, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 17, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 18, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  { position: 19, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
 
 @Component({
   selector: 'app-root',
@@ -15,18 +44,17 @@ export class AppComponent {
   modalPwaEvent: any;
   modalPwaPlatform: string | undefined;
 
-  constructor(
-    private platform: Platform,
-    private swUpdate: SwUpdate,
-    private primengConfig: PrimeNGConfig
-  ) {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataToDisplay = [...ELEMENT_DATA];
+
+  dataSource = new ExampleDataSource(this.dataToDisplay);
+
+  constructor(private platform: Platform, private swUpdate: SwUpdate) {
     this.isOnline = false;
     this.modalVersion = false;
   }
 
   public ngOnInit(): void {
-    this.primengConfig.ripple = true;
-
     this.updateOnlineStatus();
 
     window.addEventListener('online', this.updateOnlineStatus.bind(this));
@@ -89,5 +117,38 @@ export class AppComponent {
 
   public closePwa(): void {
     this.modalPwaPlatform = undefined;
+  }
+
+  addData() {
+    const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
+    this.dataToDisplay = [
+      ...this.dataToDisplay,
+      ELEMENT_DATA[randomElementIndex],
+    ];
+    this.dataSource.setData(this.dataToDisplay);
+  }
+
+  removeData() {
+    this.dataToDisplay = this.dataToDisplay.slice(0, -1);
+    this.dataSource.setData(this.dataToDisplay);
+  }
+}
+
+class ExampleDataSource extends DataSource<PeriodicElement> {
+  private _dataStream = new ReplaySubject<PeriodicElement[]>();
+
+  constructor(initialData: PeriodicElement[]) {
+    super();
+    this.setData(initialData);
+  }
+
+  connect(): Observable<PeriodicElement[]> {
+    return this._dataStream;
+  }
+
+  disconnect() {}
+
+  setData(data: PeriodicElement[]) {
+    this._dataStream.next(data);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,21 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AccordionModule } from 'primeng/accordion';
-import { TableModule } from 'primeng/table';
-import { BottomNavBarComponent } from './components/bottom-nav-bar/bottom-nav-bar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
-  declarations: [AppComponent, BottomNavBarComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
@@ -22,8 +28,6 @@ import { BottomNavBarComponent } from './components/bottom-nav-bar/bottom-nav-ba
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    AccordionModule,
-    TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
